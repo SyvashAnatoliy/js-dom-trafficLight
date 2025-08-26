2088cc7926bc1a66e90cb00c07ce8e2e120a9d7c
SyvashAnatoliy <a.sivash.ps4@gmail.com>
2025-08-26 22:18:45 +0300

    add more functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,33 @@
+const turnOn = document.createElement('button');
+turnOn.style.marginRight = '10px';
+turnOn.textContent = 'Turn On';
+document.body.appendChild(turnOn);
+
+const turnOff = document.createElement('button');
+turnOff.style.marginRight = '10px';
+turnOff.textContent = 'Turn Off';
+document.body.appendChild(turnOff);
+
 const changeColorButton = document.createElement('button');
+changeColorButton.style.marginRight = '10px';
 changeColorButton.textContent = 'Change Color';
 document.body.appendChild(changeColorButton);
 
-let currentColor = 'red';
+const turnOnTrafficLightButton = document.createElement('button');
+turnOnTrafficLightButton.style.marginRight = '10px';
+turnOnTrafficLightButton.textContent = 'Start Traffic Light';
+document.body.appendChild(turnOnTrafficLightButton);
+
+const turnOffTrafficLightButton = document.createElement('button');
+turnOffTrafficLightButton.textContent = 'Stop Traffic Light';
+document.body.appendChild(turnOffTrafficLightButton);
+
+
+
+let currentColor = 'transparent';
 
 const redLight = document.createElement('div');
+redLight.style.marginTop = '10px';
 redLight.className = 'light-red';
 redLight.style.width = '100px';
 redLight.style.height = '100px';
@@ -31,6 +54,22 @@ greenLight.style.border = '1px solid black';
 greenLight.style.borderRadius = '50%';
 document.body.appendChild(greenLight);
 
+turnOn.addEventListener('click', () => {
+  redLight.style.backgroundColor = 'red';
+  yellowLight.style.backgroundColor = 'transparent';
+  greenLight.style.backgroundColor = 'transparent';
+  currentColor = 'red';
+  isTrafficLightOn = true;
+});
+
+turnOff.addEventListener('click', () => {
+  redLight.style.backgroundColor = 'transparent';
+  yellowLight.style.backgroundColor = 'transparent';
+  greenLight.style.backgroundColor = 'transparent';
+  currentColor = 'transparent';
+  isTrafficLightOn = false;
+});
+
 changeColorButton.addEventListener('click', () => {
   if (currentColor === 'red') {
     redLight.style.backgroundColor = 'transparent';
@@ -48,4 +87,34 @@ changeColorButton.addEventListener('click', () => {
     greenLight.style.backgroundColor = 'transparent';
     currentColor = 'red';
   }
+});
+
+turnOnTrafficLightButton.addEventListener('click', () => {
+  redLight.style.backgroundColor = 'transparent';
+  yellowLight.style.backgroundColor = 'transparent';
+  greenLight.style.backgroundColor = 'transparent';
+
+  if (isTrafficLightOn === true) {
+    setTimeout(() => {
+      redLight.style.backgroundColor = 'red';
+
+      start = setInterval(() => {
+        changeColorButton.click();
+      }, 1000);
+    }, 1000);
+  }
+});
+
+turnOffTrafficLightButton.addEventListener('click', () => {
+  redLight.style.backgroundColor = 'transparent';
+  yellowLight.style.backgroundColor = 'transparent';
+  greenLight.style.backgroundColor = 'transparent';
+  clearInterval(start);
+
+  stop = setTimeout(() => {
+    currentColor = 'red';
+    redLight.style.backgroundColor = 'red';
+    yellowLight.style.backgroundColor = 'transparent';
+    greenLight.style.backgroundColor = 'transparent';
+  }, 1000);
 });
